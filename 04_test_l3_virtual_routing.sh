905c2c31f49b4108ae36ca387f3eb70093e40e60
Zhongyue Luo <zhongyue.nah@intel.com>
2013-04-16 20:33:39 +0800

    Add L3 Virtual Routing tests

---
+++ 04_test_l3_virtual_routing.sh
@@ -0,0 +1,40 @@
+source globals
+source functions
+
+VM_NAME=vm1_1
+PORT_NAME=port_$VM_NAME
+PORT_UUID=$(get_port_uuid $PORT_NAME)
+VM1_1_PUBIP=$(get_public_ip_on_port $PORT_UUID)
+
+VM_NAME=vm2_3
+PORT_NAME=port_$VM_NAME
+PORT_UUID=$(get_port_uuid $PORT_NAME)
+VM2_3_PUBIP=$(get_public_ip_on_port $PORT_UUID)
+
+VM1_1_PVTIP="$SUB_1_PREFIX"10
+VM2_3_PVTIP="$SUB_2_PREFIX"30
+
+# TODO(zyluo): Extract automatically from ifconfig eth0
+CONTROLLER_NODE_IP=192.168.6.209
+
+echo "Pinging from "$VM1_1_PUBIP" ("$VM1_1_PVTIP") to"\
+     $VM2_3_PUBIP" ("$VM2_3_PVTIP")"
+
+ssh -t ubuntu@"$VM1_1_PUBIP" 'ping '"$VM2_3_PUBIP"' -c 3'
+
+echo "Pinging from "$VM2_3_PUBIP" ("$VM2_3_PVTIP") to"\
+     $VM1_1_PUBIP" ("$VM1_1_PVTIP")"
+
+ssh -t ubuntu@"$VM2_3_PUBIP" 'ping '"$VM1_1_PUBIP"' -c 3'
+
+echo "Showing controller node IP"
+
+ifconfig eth0
+
+echo "Pinging from "$VM1_1_PUBIP" ("$VM1_1_PVTIP") to "$CONTROLLER_NODE_IP
+
+ssh -t ubuntu@"$VM1_1_PUBIP" 'ping '"$CONTROLLER_NODE_IP"' -c 3'
+
+echo "Pinging from "$CONTROLLER_NODE_IP" to "$VM1_1_PUBIP" ("$VM1_1_PVTIP")"
+
+ping $VM1_1_PUBIP -c 3
